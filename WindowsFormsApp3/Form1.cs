40717a3e294c9dd46682e5318aaab0b360cc782d
”Saniok2003” <sasha06ura@gmail.com>
2020-10-25 20:37:39 +0300

    first commit

---
+++ WindowsFormsApp3/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        int poputki = 0;
+        int min = 0, max = 101;
+        Random rand = new Random();
+        int pinky = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            pinky = rand.Next(min, max);
+            label2.Text = pinky.ToString();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            min = Convert.ToInt32(label2.Text);
+            pinky = rand.Next(min, max);
+            label2.Text = pinky.ToString();
+            ++poputki;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            max = Convert.ToInt32(label2.Text);
+            pinky = rand.Next(min, max);
+            label2.Text = pinky.ToString();
+            ++poputki;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show($"Комп він вгадав твоє число(цифру) :) 1!!!1!!1\nPoputki: {poputki}","Tirania(error)");
+        }
+    }
+}
